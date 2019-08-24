475ebfb265fa6ca95d37c0f6e6249d51ffce0736
slbarbieri <sergioluisbarbieri@gmail.com>
2019-08-24 19:09:22 -0300

    CORS

--- src/main/java/br/com/drummond/resources/StudentResources.java
+++ src/main/java/br/com/drummond/resources/StudentResources.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.drummond.domain.Student;
 import br.com.drummond.repository.StudentRepository;
 
+//@CrossOrigin
 @RestController
 public class StudentResources {
 	
